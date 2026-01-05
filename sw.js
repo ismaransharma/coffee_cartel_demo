b0b82727e6a4cb0f6cfa600dbd57d96019cf7b07
Smaran Sharma <ismaransharma.14@gmail.com>
2026-01-05 16:09:45 +0545

    Added sw.js and change in style

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = "cafe-pos-v1";
+
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./script.js",
+  "./manifest.json",
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css",
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js",
+  "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+];
+
+// Install
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+// Activate
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+});
+
+// Fetch
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then(
+      (response) => response || fetch(event.request)
+    )
+  );
+});
